e39a21aae11230bfacf6ec06cbf861aae055f738
CeadeS <martinHofmann@gmx.com>
2023-07-03 09:16:09 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytorch_imagezipdataset',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/CeadeS/ImageZipDataset',
+    license='BSD-3-Clause License',
+    author='Martin Hofmann',
+    author_email='Martin.Hofmann@tu-ilmenau.de',
+    description='Loading Images from Zipfiles Directly.',
+    install_requires=[
+       'numpy',
+       'torch',
+       'Pillow'
+       ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
